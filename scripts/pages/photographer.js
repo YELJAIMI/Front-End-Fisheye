41e2270fc0914efbd48d0f2f6c71d8d8baf05461
YELJAIMI <94940363+YELJAIMI@users.noreply.github.com>
2022-08-31 21:39:18 +0200

    lightbox.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -91,6 +91,7 @@ export function displayMedia(media){
         lienImg.className = 'lienimg';
         lienImg.setAttribute("href",media[i].image);   
         lienImg.setAttribute("title",media[i].title);
+        mediaImg.setAttribute('src',"/Front-End-Fisheye/assets/images/" +media[i].image+"");
         lienImg.setAttribute("aria-label",media[i].title);
         let mediaImg = null;
         if(media[i].image){

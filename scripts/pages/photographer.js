0927f4c2e972277bf43a4bbb0bc2423ffcd7205f
YELJAIMI <94940363+YELJAIMI@users.noreply.github.com>
2022-09-01 10:59:37 +0200

    photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -89,7 +89,7 @@ export function displayMedia(media){
         mediaDiv.className = 'media';
         const lienImg = document.createElement('a');
         lienImg.className = 'lienimg';
-        lienImg.setAttribute("href",media[i].image);   
+        lienImg.setAttribute("href",media[i].src);   
         lienImg.setAttribute("title",media[i].title);
         lienImg.setAttribute("aria-label",media[i].title);
         let mediaImg = null;

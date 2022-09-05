889c8affd94e5da522a623566c42f13f8cd7ea8f
YELJAIMI <94940363+YELJAIMI@users.noreply.github.com>
2022-09-05 12:12:06 +0200

    lightbox.js

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -12,8 +12,8 @@ export function initlightbox(){
         //new lightbox(e.currentTarget.getAttribute('href'))
         lightbox.style.display = 'flex';
 
-        function clonelink(links){
-            let format = links.src.split('.');
+        function clonelink(link){
+            let format = link.src.split('.');
             let med = null;
             if (format.at(-1) == 'mp4') {
                 med = document.createElement('video');
